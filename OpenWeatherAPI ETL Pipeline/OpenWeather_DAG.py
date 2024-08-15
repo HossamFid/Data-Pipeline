a39477ffd7e5f1abffcb672fcd4d85820633dcfd
HossamFid <49218894+HossamFid@users.noreply.github.com>
2024-08-16 01:27:14 +0300

    new operators and modifications
    add new operators and modifications to the workflow

--- OpenWeatherAPI ETL Pipeline/OpenWeather_DAG.py
+++ OpenWeatherAPI ETL Pipeline/OpenWeather_DAG.py
@@ -1,17 +1,25 @@
 from airflow import DAG
-
 from airflow.providers.http.sensors.http import HttpSensor
-from datetime import datetime, timedelta
 from airflow.operators.python import PythonOperator
+from airflow.operators.bash import BashOperator 
+
+#from airflow.providers.smtp.operators.smtp import SmtpEmailOperator
+
+from datetime import datetime, timedelta
 from OpenWeatherETL import weather_ETL
+import cities
+
+
+
 default_args = {
     'owner': 'Hossam',
     'depends_on_past': False,
     'start_date': datetime(2024, 8, 14),
-    'email': ['mrhossamfid@gmail.com'],
+    'email': ['samxfid@gmail.com'],
     'email_on_failure': True,
     'email_on_retry': True,
-    'retries': 3,
+    'email_on_success': True,
+    'retries': 1,
     'retry_delay': timedelta(minutes=2),
 }
 
@@ -22,16 +30,25 @@ with DAG(
     catchup=False,
 ) as dag:
 
+
+
     def starter():
-        airflow = """
-        __  _____  _____  __  __
-        /\\ \\/ / __ \\/__   \\/\\ \\/ /
-        \\ \\  /\\ \\ \\/ _\\ \\  \\ \\  / 
-        \\ \\_\\ \\__/\\ \\__/   \\_\\_\\ 
-        \\/_/\\____/\\____/\\/_/\\_\\
+        apache_airflow_job = """
+        AAAAA   PPPPP    AAAAA    CCCCC   H    H  EEEEE           AAAAA  III  RRRRR   FFFFF  L       OOOOO  W     W
+        A     A  P    P  A     A  C        H    H  E               A     A  I   R    R  F      L      O     O  W     W
+        AAAAAAA  PPPPP   AAAAAAA  C        HHHHHH  EEEEE           AAAAAAA  I   RRRRR   FFFFF  L      O     O  W  W  W
+        A     A  P       A     A  C        H    H  E               A     A  I   R    R  F      L      O     O   W W W
+        A     A  P       A     A   CCCCC   H    H  EEEEE           A     A  III  R    R  F      LLLLL   OOOOO     W W
         """
-        print(airflow)
-        print("Airflow job started working!")
+
+        hossam_fid_signature = """
+        Written by Hossam Fid
+        """
+
+        print(apache_airflow_job)
+        print(hossam_fid_signature)
+        print("Apache Airflow job started working! .........")
+
 
     job_start = PythonOperator(
         task_id='job_start',
@@ -46,6 +63,27 @@ with DAG(
 
     )
 
+    # read_cities = BashOperator(
+    #     task_id = 'read_cities',
+    #     bash_command='cat cities.txt'
+
+    # )
+
+
+# ... other code in your DAG ...
+
+    # send_email_task = SmtpEmailOperator(
+    # task_id='send_email',
+    # to='samxfid@gmail.com',  # Replace with your recipient email address
+    # # cc=[],  # Optional: List of CC email addresses
+    # # bcc=[],  # Optional: List of BCC email addresses
+    # subject='Airflow Notification',
+    # html_content='This is an email notification from Airflow.',
+    # mime_type='text/html',  # Set MIME type (text/plain for plain text)
+    # from_email='penfinaltest@gmail.com',
+    # smtp_conn_id='smtp_default'  # Set the SMTP connection ID (explained later)
+    # )
+
     api_key = 'c0810f4a8783c59ca8ccd3ce01949653'
 
     get_weather_data= PythonOperator(
@@ -54,4 +92,4 @@ with DAG(
     )
 
 
-    job_start >> is_api_200OK #>> get_weather_data
+    job_start >> is_api_200OK >>get_weather_data #>> send_email_task # 
